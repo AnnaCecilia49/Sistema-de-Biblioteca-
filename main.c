6a808d488f0a377339a6c02f294778306f4bd2cb
The Lord of Cheese <70858169+cheesewizard01@users.noreply.github.com>
2026-09-20 14:17:29 -0300

    create main.c with basic content

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const char* coming_soon =
+"A library system that does something. Coming soon!";
+
+int main(int argc, char *argv[]) {
+    printf("%s\n", coming_soon);
+    return EXIT_SUCCESS;
+}
